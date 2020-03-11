15455b8db071bd7d1073a8452dc358f6c30be252
Renan0liveira <renanromaooliveira@gmail.com>
2020-03-10 21:54:32 -0300

    Correçao do app

--- lib/main.dart
+++ lib/main.dart
@@ -170,7 +170,7 @@ class _HomeState extends State<Home> {
                             shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
                           ),
                           Divider(),
-                          floatingActionButton: FloatingActionButton(
+                          FloatingActionButton(
                             child: Icon(Icons.add),
                             onPressed: () {},
                           )
